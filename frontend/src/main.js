420dfc82b8d103a90fd3409be23805d3b1c85fcd
Oleg Zhuravko <o.zhuravko@gmail.com>
2023-06-29 22:58:01 +0300

    add formKit, moved styles for "content-wrapper" to common file

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
+import { plugin, defaultConfig } from "@formkit/vue";
 import { createHead } from "@vueuse/head";
 
 import "./styles/main.scss";
 
-createApp(App).use(router).use(createHead()).mount("#app");
+createApp(App)
+  .use(router)
+  .use(createHead())
+  .use(plugin, defaultConfig)
+  .mount("#app");
--- frontend/src/pages/PartRequest.vue
+++ frontend/src/pages/PartRequest.vue
@@ -1,6 +1,41 @@
 <template>
   <MainLayout>
-    <div>Part request</div>
+    <div class="content-wrapper">
+      <h3>Part request</h3>
+
+      <FormKit type="group">
+        <FormKit
+          type="text"
+          label="Street address"
+          name="street"
+          placeholder="Your street"
+          validation="required"
+        />
+        <FormKit
+          type="text"
+          label="City"
+          placeholder="Your city"
+          name="city"
+          validation="required"
+        />
+        <div class="double">
+          <FormKit
+            type="select"
+            label="State"
+            name="state"
+            placeholder="Your state"
+            validation="required"
+          />
+          <FormKit
+            type="text"
+            name="zip"
+            label="Zip"
+            placeholder="Your zip"
+            validation="required|number"
+          />
+        </div>
+      </FormKit>
+    </div>
   </MainLayout>
 </template>
 
@@ -9,5 +44,5 @@ import MainLayout from "@/layouts/MainLayout";
 </script>
 
 <style lang="sass" scoped>
-@import "@/styles/pages/_home-page.scss"
+@import "@/styles/pages/_part-request.scss"
 </style>
