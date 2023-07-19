4022717b558022ae70f65f6c01fdfad49fbf04cf
Oleg Zhuravko <olegkhazo@gmail.com>
2023-07-19 23:24:33 +0300

    updated auth/login/mailer code

--- server/config/default.ts
+++ server/config/default.ts
@@ -1,5 +1,14 @@
 export default {
-    port: 3000,
-    dbUri: "mongodb://localhost:27017/my-car-parts",
-    logLevel: "info",
-}
+  port: 3000,
+  dbUri: "mongodb://localhost:27017/my-car-parts",
+  logLevel: "info",
+  accessTokenPrivateKey: "",
+  refreshTokenPrivateKey: "",
+  smtp: {
+    user: "icqskc5mxzf3bwct@ethereal.email",
+    pass: "mVSU79SbF3WmvGqy4V",
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+  },
+};
