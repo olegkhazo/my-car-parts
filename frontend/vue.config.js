176e137da2ccf9cf339ef1c036635a383dbbd2d4
Oleg Zhuravko <olegkhazo@gmail.com>
2023-08-06 14:44:37 +0300

    add redirect to /api for backendin  vue.config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -19,5 +19,25 @@ module.exports = {
         additionalData: `@import "~@/styles/variables";`,
       },
     },
+  },
+
+  // baseUrl: IS_PRODUCTION
+  // ? 'http://cdn123.com'
+  // : '/',
+  // For Production, replace set baseUrl to CDN
+  // And set the CDN origin to `yourdomain.com/static`
+  // Whitenoise will serve once to CDN which will then cache
+  // and distribute
+  devServer: {
+    proxy: {
+      "/api*": {
+        // Forward frontend dev server request for /api to django dev server
+        target: "http://localhost:3000/",
+      },
+      // "/admin-*": {
+        // Forward frontend dev server request for /api to django dev server
+        // target: "http://localhost:3000/",
+      // },
+    },
   }
 };
