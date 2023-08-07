f645dd8fd4bb22a038957fad1488cb3678495c00
Oleg Zhuravko <olegkhazo@gmail.com>
2023-08-07 09:59:44 +0300

    add to index partRequestForm route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import user from "./user.routes";
 import auth from "./auth.routes";
+import partRequestForm from "./partRequestForm.routes";
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.get("/healthcheck", (_, res) => res.sendStatus(200));
 
 router.use(user);
 router.use(auth);
+router.use(partRequestForm);
 
 export default router;
