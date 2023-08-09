59a400cd3a436be0878a3f48f9af47a5c457de63
Oleg Zhuravko <olegkhazo@gmail.com>
2023-08-09 22:23:57 +0300

    add allPartRequestsPage, styling for it, update README.md for frontend and backend

---
+++ frontend/src/pages/AllSparePartRequestsPage.vue
@@ -0,0 +1,128 @@
+<template>
+  <MainLayout>
+    <div class="content-wrapper">
+      <h2>All User Requests</h2>
+      <div class="last-requests-table-wrapper">
+        <div class="table-pre-header"></div>
+        <div class="table-wrapper">
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Car part</th>
+                <th>Area/Region</th>
+                <th>Sent</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <img
+                    src="@/assets/images/green-wheel.svg"
+                    alt=""
+                  />
+                </td>
+                <td>Rear silencer for 2008 Volkswagen Multivan 3.2 petrol</td>
+                <td>Orlando</td>
+                <td>2 minutes ago</td>
+              </tr>
+              <tr>
+                <td>
+                  <img
+                    src="@/assets/images/green-wheel.svg"
+                    alt=""
+                  />
+                </td>
+                <td>Rear silencer for 2008 Volkswagen Multivan 3.2 petrol</td>
+                <td>Orlando</td>
+                <td>2 minutes ago</td>
+              </tr>
+              <tr>
+                <td>
+                  <img
+                    src="@/assets/images/green-wheel.svg"
+                    alt=""
+                  />
+                </td>
+                <td>Rear silencer for 2008 Volkswagen Multivan 3.2 petrol</td>
+                <td>Orlando</td>
+                <td>2 minutes ago</td>
+              </tr>
+              <tr>
+                <td>
+                  <img
+                    src="@/assets/images/green-wheel.svg"
+                    alt=""
+                  />
+                </td>
+                <td>Rear silencer for 2008 Volkswagen Multivan 3.2 petrol</td>
+                <td>Orlando</td>
+                <td>2 minutes ago</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="last-requests-sm-screen-wrapper">
+          <ul>
+            <li>
+              <div class="single-request-wrapper">
+                <img
+                  src="@/assets/images/green-wheel.svg"
+                  alt=""
+                />
+                <div class="request-description">
+                  <p>Rear silencer for 2008 Volkswagen Multivan 3.2 petrol</p>
+                  <span>Orlando - 2 minutes ago</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="single-request-wrapper">
+                <img
+                  src="@/assets/images/green-wheel.svg"
+                  alt=""
+                />
+                <div class="request-description">
+                  <p>Rear silencer for 2008 Volkswagen Multivan 3.2 petrol</p>
+                  <span>Orlando - 2 minutes ago</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="single-request-wrapper">
+                <img
+                  src="@/assets/images/green-wheel.svg"
+                  alt=""
+                />
+                <div class="request-description">
+                  <p>Rear silencer for 2008 Volkswagen Multivan 3.2 petrol</p>
+                  <span>Orlando - 2 minutes ago</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="single-request-wrapper">
+                <img
+                  src="@/assets/images/green-wheel.svg"
+                  alt=""
+                />
+                <div class="request-description">
+                  <p>Rear silencer for 2008 Volkswagen Multivan 3.2 petrol</p>
+                  <span>Orlando - 2 minutes ago</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from "@/layouts/MainLayout.vue";
+</script>
+
+<style lang="sass" scoped>
+@import "@/styles/pages/_all-spare-part-requests-page.scss"
+</style>
---
+++ server/README.md
@@ -0,0 +1,13 @@
+# my-car-part-server
+
+## Project setup
+```
+yarn install
+```
+
+### Setup MongoDB, use MongoDB compass
+
+### Launch the backend
+```
+yarn dev
+```
