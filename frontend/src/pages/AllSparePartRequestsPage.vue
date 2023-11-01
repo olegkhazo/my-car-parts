617d668557343e3f3f397a5c3ad6d32707530e12
Oleg Zhuravko <olegkhazo@gmail.com>
2023-11-01 16:32:49 +0200

    [WIP] All spare part request  mobile view

--- frontend/src/pages/AllSparePartRequestsPage.vue
+++ frontend/src/pages/AllSparePartRequestsPage.vue
@@ -10,34 +10,27 @@
               <thead>
                 <tr>
                   <th></th>
-                  <th>Spare part description</th>
-                  <th>Area/Region</th>
-                  <th>Sent</th>
-                  <th></th>
+                  <th>Spare part</th>
+                  <th class="form-area-column">Area/Region</th>
+                  <th class="form-date-column">Sent</th>
+                  <th class="form-info-button-column"></th>
                 </tr>
               </thead>
-              <tbody
-                v-for="request in filteredPartRequestsData"
-                id="tbody"
-                :key="request._id"
-              >
-                <tr
-                  class="single-request-row"
-                  @click="showBlockWithAllRelatedContent"
-                >
+              <tbody v-for="request in filteredPartRequestsData" id="tbody" :key="request._id">
+                <tr class="single-request-row" @click="showBlockWithAllRelatedContent">
                   <td>
-                    <img
-                      src="@/assets/images/green-wheel.svg"
-                      alt=""
-                    />
+                    <img src="@/assets/images/green-wheel.svg" alt="" />
                   </td>
                   <td>
                     {{ request.part_name }} for {{ request.car_make }} {{ request.car_model }}
                     {{ request.engine_volume }} {{ request.fuel_type }}
+                    <div class="block-for-mobile-screen">
+                      {{ request.city }} - {{ getTimeAgo(request.created_date) }}
+                    </div>
                   </td>
-                  <td>{{ request.city }}</td>
-                  <td>{{ getTimeAgo(request.created_date) }}</td>
-                  <td>
+                  <td class="form-area-column">{{ request.city }}</td>
+                  <td class="form-date-column">{{ getTimeAgo(request.created_date) }}</td>
+                  <td class="form-info-button-column">
                     <a class="sm-green-btn">Info</a>
                   </td>
                 </tr>
@@ -45,71 +38,77 @@
                   <td colspan="5">
                     <div class="all-content-wrapper">
                       <div class="all-content">
-                        <span v-if="request.part_name">
-                          Spare part: <span class="bold">{{ request.part_name }}</span>
-                        </span>
-                        <span v-if="request.part_group">
-                          Group: <span class="bold">{{ request.part_group }}</span>
-                        </span>
-                        <span v-if="request.type_of_part">
-                          Type of part: <span class="bold">{{ request.type_of_part }}</span>
-                        </span>
-                        <span v-if="request.part_condition">
-                          Condition: <span class="bold">{{ request.part_condition }}</span>
-                        </span>
-                        <span v-if="request.part_code">
-                          Part code: <span class="bold">{{ request.part_code }}</span>
-                        </span>
-                        <span v-if="request.car_type">
-                          Car type: <span class="bold">{{ request.car_type }}</span>
-                        </span>
-                        <span v-if="request.car_make">
-                          Car make: <span class="bold">{{ request.car_make }}</span>
-                        </span>
-                        <span v-if="request.car_model">
-                          Car model: <span class="bold">{{ request.car_model }}</span>
-                        </span>
-                        <span v-if="request.car_year">
-                          Car year: <span class="bold">{{ request.car_year }}</span>
-                        </span>
-                        <span v-if="request.fuel_type">
-                          Fuel type: <span class="bold">{{ request.fuel_type }}</span>
-                        </span>
-                        <span v-if="request.engine_volume">
-                          Engine volume: <span class="bold">{{ request.engine_volume }}</span>
-                        </span>
-                        <span v-if="request.car_body">
-                          Car body: <span class="bold">{{ request.car_body }}</span>
-                        </span>
-                        <span v-if="request.vin_code">
-                          VIN code: <span class="bold">{{ request.vin_code }}</span>
-                        </span>
-                        <span v-if="request.comment">
-                          User comment: <span class="bold">{{ request.comment }}</span>
-                        </span>
-                        <span v-if="request.city">
-                          State/City: <span class="bold">{{ request.city }}</span>
-                        </span>
-                        <span v-if="request.email">
-                          Email: <span class="bold">{{ request.email }}</span>
-                        </span>
-                        <span v-if="request.phone">
-                          Phone: <span class="bold">{{ request.phone }}</span>
-                        </span>
-                        <span v-if="request.name">
-                          Name: <span class="bold">{{ request.name }}</span>
-                        </span>
+                        <div class="single-request-details">
+                          <div class="single-request-details-left">
+                            <span v-if="request.part_name">
+                              Spare part: <span class="bold">{{ request.part_name }}</span>
+                            </span>
+                            <span v-if="request.part_group">
+                              Group: <span class="bold">{{ request.part_group }}</span>
+                            </span>
+                            <span v-if="request.type_of_part">
+                              Type of part: <span class="bold">{{ request.type_of_part }}</span>
+                            </span>
+                            <span v-if="request.part_condition">
+                              Condition: <span class="bold">{{ request.part_condition }}</span>
+                            </span>
+                            <span v-if="request.part_code">
+                              Part code: <span class="bold">{{ request.part_code }}</span>
+                            </span>
+                            <span v-if="request.car_type">
+                              Car type: <span class="bold">{{ request.car_type }}</span>
+                            </span>
+                            <span v-if="request.car_make">
+                              Car make: <span class="bold">{{ request.car_make }}</span>
+                            </span>
+                            <span v-if="request.car_model">
+                              Car model: <span class="bold">{{ request.car_model }}</span>
+                            </span>
+                            <span v-if="request.car_year">
+                              Car year: <span class="bold">{{ request.car_year }}</span>
+                            </span>
+                          </div>
+                          <div class="single-request-details-right">
+                            <span v-if="request.fuel_type">
+                              Fuel type: <span class="bold">{{ request.fuel_type }}</span>
+                            </span>
+                            <span v-if="request.engine_volume">
+                              Engine volume: <span class="bold">{{ request.engine_volume }}</span>
+                            </span>
+                            <span v-if="request.car_body">
+                              Car body: <span class="bold">{{ request.car_body }}</span>
+                            </span>
+                            <span v-if="request.vin_code">
+                              VIN code: <span class="bold">{{ request.vin_code }}</span>
+                            </span>
+                            <span v-if="request.comment">
+                              User comment: <span class="bold">{{ request.comment }}</span>
+                            </span>
+                            <span v-if="request.city">
+                              State/City: <span class="bold">{{ request.city }}</span>
+                            </span>
+                            <span v-if="request.email">
+                              Email: <span class="bold">{{ request.email }}</span>
+                            </span>
+                            <span v-if="request.phone">
+                              Phone: <span class="bold">{{ request.phone }}</span>
+                            </span>
+                            <span v-if="request.name">
+                              Name: <span class="bold">{{ request.name }}</span>
+                            </span>
+                          </div>
+                        </div>
 
-                        <router-link
-                          :to="{ name: 'offer-page', params: { requestId: request._id } }"
-                          class="suggest-button xl-green-btn"
-                          >Suggest your variant</router-link
-                        >
-                        <span
-                          class="close-content-btn blue-btn"
-                          @click="hideOpenedContentByButtonClick"
-                          >Hide content</span
-                        >
+                        <div class="spare-part-requests-btn-wrapper">
+                          <router-link
+                            :to="{ name: 'offer-page', params: { requestId: request._id } }"
+                            class="suggest-button xl-green-btn"
+                            >Suggest your variant</router-link
+                          >
+                          <span class="close-content-btn blue-btn" @click="hideOpenedContentByButtonClick"
+                            >Hide content</span
+                          >
+                        </div>
                       </div>
                     </div>
                   </td>
