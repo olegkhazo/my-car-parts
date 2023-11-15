a3ce26e3acf6c1acf466d08360962d2a9c640eb7
Oleg Zhuravko <olegkhazo@gmail.com>
2023-11-15 13:50:25 +0200

    terms page

---
+++ client/pages/terms.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="content-wrapper">
+    <div class="markdown prose w-full break-words dark:prose-invert light">
+      <h2>Terms of Service</h2>
+      <p>
+        Welcome to MyNextParts, your go-to platform for connecting buyers and sellers of automotive parts in the United
+        States. Before you begin using our service, please read and agree to the following terms:
+      </p>
+      <p>
+        <strong>1. Purpose of the Service</strong> MyNextParts serves as a free online platform where individuals
+        seeking automotive parts ("Buyers") can submit requests, and sellers of automotive parts ("Sellers") can view
+        and respond to these requests. We do not sell any parts directly; instead, we facilitate the connection between
+        Buyers and Sellers.
+      </p>
+      <p><strong>2. User Responsibilities</strong></p>
+      <ul>
+        <li>
+          <strong>Buyers:</strong> By submitting a request, you agree to provide accurate and detailed information about
+          the parts you are searching for and your contact details.
+        </li>
+        <li>
+          <strong>Sellers:</strong> When responding to a request, Sellers agree to provide accurate information about
+          the availability, pricing, and terms of sale for their automotive parts.
+        </li>
+      </ul>
+      <p><strong>3. Platform Usage</strong></p>
+      <ul>
+        <li>
+          MyNextParts is free for Buyers. Sellers may be subject to fees or commissions, as outlined in separate
+          agreements.
+        </li>
+        <li>
+          Users must not engage in fraudulent activities, misrepresentation, or any form of misconduct on the platform.
+        </li>
+      </ul>
+      <p><strong>4. Dispute Resolution</strong></p>
+      <ul>
+        <li>MyNextParts is not responsible for the quality, accuracy, or legality of the parts offered by Sellers.</li>
+        <li>Any disputes between Buyers and Sellers must be resolved directly between the parties involved.</li>
+      </ul>
+      <p><strong>5. Privacy</strong></p>
+      <ul>
+        <li>MyNextParts collects and processes personal information in accordance with our Privacy Policy.</li>
+      </ul>
+      <p><strong>6. Termination of Service</strong></p>
+      <ul>
+        <li>
+          MyNextParts reserves the right to terminate or suspend user accounts that violate these terms or engage in any
+          form of abuse or misuse of the platform.
+        </li>
+      </ul>
+      <p><strong>7. Limitation of Liability</strong></p>
+      <ul>
+        <li>
+          MyNextParts is not liable for any direct, indirect, incidental, special, or consequential damages resulting
+          from the use of our platform.
+        </li>
+      </ul>
+      <p>
+        By using MyNextParts, you agree to these terms. We reserve the right to update or modify these terms at any
+        time. It is your responsibility to review them periodically for changes. If you do not agree with these terms,
+        please do not use our service.
+      </p>
+    </div>
+    <br />
+    <NuxtLink class="light-gray-btn" to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.content-wrapper {
+  min-height: 60vh;
+}
+</style>
