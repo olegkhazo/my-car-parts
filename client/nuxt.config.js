efa60f9a857b958d2e1aa75c9b12deb8d012a66b
Oleg Zhuravko <o.zhuravko@gmail.com>
2023-11-28 10:03:16 +0200

    google analytics updates

---
+++ client/nuxt.config.js
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config';
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+ 
+  css: [
+    "@/assets/styles/main.scss",
+  ],
+  modules: [
+    "@pinia/nuxt", 
+    "@nuxtjs/style-resources", 
+    "nuxt-gtag",
+    "@nuxt/image", 
+    "nuxt-simple-robots",  
+    "nuxt-simple-sitemap"
+  ],
+  site: {
+    url: 'https://mynextparts.com',
+  },
+  gtag: {
+    id: "G-LRYNR27VT7"
+  }
+})
+
