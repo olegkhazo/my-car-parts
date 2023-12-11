23979b803adee8c68964da2af154d9d43935903e
Oleg Zhuravko <o.zhuravko@gmail.com>
2023-12-11 17:42:09 +0200

    postchog

--- client/app.vue
+++ client/app.vue
@@ -7,3 +7,9 @@
     <TheFooter />
   </div>
 </template>
+
+<script setup>
+import posthog from "posthog-js";
+
+posthog.init("phc_WDTGC09yj0oPOYDgvFnWRC46QuJMzRpKJnCCndeJskW", { api_host: "https://eu.posthog.com" });
+</script>
