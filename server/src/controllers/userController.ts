b964034866d8983c6502d358e6af5f49af2f0671
Oleg Zhuravko <o.zhuravko@gmail.com>
2023-12-24 12:43:21 +0200

    [WIP] registratio - empty email activation

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,7 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 import { UsersModel } from '../models/user.models';
-// import { sendPartRequestDetails } from '../services/mailer.service';
+import { sendUsersAccountActivationLink } from '../services/mailer.service';
+
 const jwt = require('jsonwebtoken');
+const API_HOST = process.env.API_HOST;
 
 export const registerUser = async (req: Request, res: Response, next: NextFunction) => {
     const { userEmail, password } = req.body;
@@ -11,7 +13,7 @@ export const registerUser = async (req: Request, res: Response, next: NextFuncti
   
       if (existingUser) {
         console.log("already exist");
-        return res.status(400).json({ error: 'Пользователь таким именем уже существует' });
+        return res.status(400).json({ error: 'User with such email already exist' });
       }
   
       const activationToken = jwt.sign({ userEmail }, 'activation-secret-key', { expiresIn: '1h' });
@@ -20,19 +22,22 @@ export const registerUser = async (req: Request, res: Response, next: NextFuncti
       const newUser = new UsersModel({ userEmail, password, activationToken });
       await newUser.save();
   
-      //const activationLink = `http://localhost:3000/api/activate/${activationToken}`;
-      //const activationLink = `http://localhost:3000/activate/${activationToken}`;
+      const activationLink = `${API_HOST}/activate/${activationToken}`;
+
+
+      //await sendPartOfferNotification(req.body.email, req.body.part_name, req.body);
+
+      await sendUsersAccountActivationLink(req.body.userEmail, activationLink)
 
       // const mailOptions = {
       //   from: 'your-email@gmail.com',
       //   to: 'user-email@example.com',
       //   subject: 'Активация аккаунта',
       //   text: `Для активации вашего аккаунта перейдите по следующей ссылке: ${activationLink}`,
-      // };
-  
-      // await transporter.sendMail(mailOptions);
-  
+      // };  
+      // await transporter.sendMail(mailOptions);  
       // res.status(201).json({ message: 'Письмо с инструкциями по активации отправлено' });
+
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Что-то пошло не так' });
