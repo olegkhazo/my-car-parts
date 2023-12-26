6c3d5d90aeee4552a738d07f41768050a2cafd85
Oleg Zhuravko <o.zhuravko@gmail.com>
2023-12-26 11:09:07 +0200

    [WIP] layout for activation-success page

--- client/pages/registration/activation-success.vue
+++ client/pages/registration/activation-success.vue
@@ -1,14 +1,57 @@
 <template>
   <div class="content-wrapper activating-screen">
-    <h2>Account Activating</h2>
-    <div>Your Account activated succesful</div>
+    <h1>Account Activated</h1>
+    <div>
+      <p>
+        Congratulation! Your Account activated succesful. Now you can use your account and use the service to the
+        fullest.
+      </p>
+    </div>
   </div>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
+@import "@/assets/styles/_variables.scss";
+
 .activating-screen {
-  height: 500px;
+  padding: 0 0 20vh 0;
+  h1 {
+    font-size: 40px;
+    margin-bottom: 40px;
+    font-weight: 300;
+    text-align: center;
+
+    @media (max-width: 834px) {
+      font-size: 28px;
+    }
+
+    @media (max-width: 768px) {
+      margin: 20px auto;
+    }
+
+    @media (max-width: 382px) {
+      font-size: 24px;
+    }
+  }
+
+  p {
+    margin-top: 30px;
+    font-size: 20px;
+
+    @media (max-width: 768px) {
+      margin-top: 20px;
+    }
+
+    @media (max-width: 382px) {
+      font-size: 18px;
+    }
+    a {
+      text-decoration: none;
+      color: $blue;
+      font-weight: 600;
+    }
+  }
 }
 </style>
