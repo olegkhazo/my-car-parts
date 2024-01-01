7d858c7bc4c08ac95e2feaafa3b7e49a2a3e6f66
Oleg Zhuravko <o.zhuravko@gmail.com>
2024-01-01 21:02:26 +0200

    [WIP] sign in layout without styles

--- client/pages/sign-in.vue
+++ client/pages/sign-in.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="content-wrapper">Sign-in page</div>
+  <div class="content-wrapper">
+    <div class="form-wrapper">
+      <h1>Sign In</h1>
+      <div class="offer-form-fields-section">
+        <span v-if="!isEmailValid && formButtonClicked" class="input-error-notification">
+          Please enter a valid email address.
+        </span>
+        <input id="email" v-model="userCreds.email" type="email" name="email" placeholder="Work email address *" />
+
+        <span v-if="!isPasswordValid && formButtonClicked" class="input-error-notification">
+          Please enter a valid password.
+        </span>
+        <input
+          id="password"
+          v-model="userCreds.password"
+          type="password"
+          name="password"
+          placeholder="Password(8 or more characters) *"
+        />
+
+        <button class="xl-green-btn" @click="signIn">Register a company</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -12,6 +35,10 @@ useHead({
     },
   ],
 });
+
+async function signIn() {
+  console.log("Sign In");
+}
 </script>
 
 <style lang="scss" scoped>
