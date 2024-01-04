3a32121a61723bee287392e9a6dd1881bad4db88
Oleg Zhuravko <o.zhuravko@gmail.com>
2024-01-04 15:38:44 +0200

    [WIP] authStore changing

--- client/stores/useAuthStore.js
+++ client/stores/useAuthStore.js
@@ -1,5 +1,4 @@
 import { defineStore } from "pinia";
-import { API_URL } from "@/utils/constants";
 
 export const useAuthStore = defineStore("auth", () => {
   const loggedIn = computed(() => {
@@ -8,6 +7,35 @@ export const useAuthStore = defineStore("auth", () => {
     }
     return false;
   });
+
+  const tokenExpiration = ref(null);
+
+  // Initialize tokenExpiration from localStorage on store creation
+  if (process.client) {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      const decodedToken = JSON.parse(atob(storedToken.split(".")[1]));
+      const expirationTime = decodedToken.exp * 1000;
+      console.log("Stored Token Expiration Time:", new Date(expirationTime));
+      tokenExpiration.value = new Date(expirationTime);
+    }
+  }
+
+  // Schedule clearing token after expiration
+  watch(
+    tokenExpiration,
+    (newExpiration, oldExpiration) => {
+      console.log("Token Expiration Changed:", newExpiration, oldExpiration);
+      if (oldExpiration !== null) {
+        const now = new Date();
+        console.log("Current Time:", now);
+        if (now >= newExpiration) {
+          clearUserInfo();
+        }
+      }
+    },
+    { immediate: true }
+  );
 
   async function login(creds) {
     const { data: authorisedUser, error } = await useFetch(API_URL + "login", {
@@ -21,11 +49,18 @@ export const useAuthStore = defineStore("auth", () => {
       // Add token to localStorage
       if (process.client) {
         localStorage.setItem("token", token);
-      }
 
+        // Parse token to get expiration time
+        const decodedToken = JSON.parse(atob(token.split(".")[1]));
+        console.log("Decoded Token:", decodedToken);
+        const expirationTime = decodedToken.exp * 1000;
+        console.log("New Token Expiration Time:", new Date(expirationTime));
+
+        // Set token expiration date
+        tokenExpiration.value = new Date(expirationTime);
+      }
     } else if (error.value) {
       console.error(error.value);
-      // Consider better ways to handle and display errors
       console.log("Something went wrong: " + error.value);
     }
   }
@@ -38,6 +73,7 @@ export const useAuthStore = defineStore("auth", () => {
     // Logout successful. Remove token
     if (process.client) {
       localStorage.removeItem("token");
+      tokenExpiration.value = null;
     }
   }
 
