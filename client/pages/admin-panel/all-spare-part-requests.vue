b3c575d4ed12ddc3ea38da849fa95d3dda7d5eff
Oleg Zhuravko <o.zhuravko@gmail.com>
2024-01-21 23:35:51 +0200

    [WIP] add calc width for main admin content block

--- client/pages/admin-panel/all-spare-part-requests.vue
+++ client/pages/admin-panel/all-spare-part-requests.vue
@@ -1,147 +1,145 @@
 <template>
-  <div class="content-wrapper">
-    <div class="all-requests-wrapper">
-      <h1>All car part requests</h1>
-      <TheFilterByCarTypes />
-      <div class="all-requests-table-wrapper">
-        <div class="table-wrapper">
-          <table>
-            <thead>
-              <tr>
-                <th></th>
-                <th>Car part</th>
-                <th class="form-area-column">Area/Region</th>
-                <th class="form-date-column">Sent</th>
-                <th class="form-info-button-column"></th>
-              </tr>
-            </thead>
-            <tbody v-for="request in chunkOfRequestsForView" id="tbody" :key="request._id">
-              <tr class="single-request-row" @click="showAllContentOfSingleRequest">
-                <td>
-                  <div class="green-wheel-img-wrapper">
-                    <NuxtImg src="/images/green-wheel.svg" title="green-wheel" />
-                  </div>
-                </td>
-                <td>
-                  {{ request.part_name }} for {{ request.car_make }} {{ request.car_model }} {{ request.engine_volume }}
-                  {{ request.fuel_type }}
-                  <div class="block-for-mobile-screen">
-                    {{ request.city }} -
-                    {{ getTimeAgo(request.created_date) }}
-                  </div>
-                </td>
-                <td class="form-area-column">{{ request.city }}</td>
-                <td class="form-date-column">
+  <div class="all-requests-wrapper">
+    <h1>All car part requests</h1>
+    <TheFilterByCarTypes />
+    <div class="all-requests-table-wrapper">
+      <div class="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Car part</th>
+              <th class="form-area-column">Area/Region</th>
+              <th class="form-date-column">Sent</th>
+              <th class="form-info-button-column"></th>
+            </tr>
+          </thead>
+          <tbody v-for="request in chunkOfRequestsForView" id="tbody" :key="request._id">
+            <tr class="single-request-row" @click="showAllContentOfSingleRequest">
+              <td>
+                <div class="green-wheel-img-wrapper">
+                  <NuxtImg src="/images/green-wheel.svg" title="green-wheel" />
+                </div>
+              </td>
+              <td>
+                {{ request.part_name }} for {{ request.car_make }} {{ request.car_model }} {{ request.engine_volume }}
+                {{ request.fuel_type }}
+                <div class="block-for-mobile-screen">
+                  {{ request.city }} -
                   {{ getTimeAgo(request.created_date) }}
-                </td>
-                <td class="form-info-button-column">
-                  <span class="sm-green-btn">Info</span>
-                </td>
-              </tr>
-              <tr class="all-content-for-single-request hidden">
-                <td colspan="5">
-                  <div class="all-content-wrapper">
-                    <div class="all-content">
-                      <div class="single-request-details">
-                        <div class="single-request-details-left">
-                          <span v-if="request.part_name">
-                            Spare part:
-                            <span class="bold">{{ request.part_name }}</span>
-                          </span>
-                          <span v-if="request.part_group">
-                            Group:
-                            <span class="bold">{{ request.part_group }}</span>
-                          </span>
-                          <span v-if="request.type_of_part">
-                            Type of part:
-                            <span class="bold">{{ request.type_of_part }}</span>
-                          </span>
-                          <span v-if="request.part_condition">
-                            Condition:
-                            <span class="bold">{{ request.part_condition }}</span>
-                          </span>
-                          <span v-if="request.part_code">
-                            Part code:
-                            <span class="bold">{{ request.part_code }}</span>
-                          </span>
-                          <span v-if="request.car_type">
-                            Car type:
-                            <span class="bold">{{ request.car_type }}</span>
-                          </span>
-                          <span v-if="request.car_make">
-                            Car make:
-                            <span class="bold">{{ request.car_make }}</span>
-                          </span>
-                          <span v-if="request.car_model">
-                            Car model:
-                            <span class="bold">{{ request.car_model }}</span>
-                          </span>
-                          <span v-if="request.car_year">
-                            Car year:
-                            <span class="bold">{{ request.car_year }}</span>
-                          </span>
-                        </div>
-                        <div class="single-request-details-right">
-                          <span v-if="request.fuel_type">
-                            Fuel type:
-                            <span class="bold">{{ request.fuel_type }}</span>
-                          </span>
-                          <span v-if="request.engine_volume">
-                            Engine volume:
-                            <span class="bold">{{ request.engine_volume }}</span>
-                          </span>
-                          <span v-if="request.car_body">
-                            Car body:
-                            <span class="bold">{{ request.car_body }}</span>
-                          </span>
-                          <span v-if="request.vin_code">
-                            VIN code:
-                            <span class="bold">{{ request.vin_code }}</span>
-                          </span>
-                          <span v-if="request.comment">
-                            User comment:
-                            <span class="bold">{{ request.comment }}</span>
-                          </span>
-                          <span v-if="request.city">
-                            State/City:
-                            <span class="bold">{{ request.city }}</span>
-                          </span>
-                          <span v-if="request.name">
-                            Name: <span class="bold">{{ request.name }}</span>
-                          </span>
-                        </div>
+                </div>
+              </td>
+              <td class="form-area-column">{{ request.city }}</td>
+              <td class="form-date-column">
+                {{ getTimeAgo(request.created_date) }}
+              </td>
+              <td class="form-info-button-column">
+                <span class="sm-green-btn">Info</span>
+              </td>
+            </tr>
+            <tr class="all-content-for-single-request hidden">
+              <td colspan="5">
+                <div class="all-content-wrapper">
+                  <div class="all-content">
+                    <div class="single-request-details">
+                      <div class="single-request-details-left">
+                        <span v-if="request.part_name">
+                          Spare part:
+                          <span class="bold">{{ request.part_name }}</span>
+                        </span>
+                        <span v-if="request.part_group">
+                          Group:
+                          <span class="bold">{{ request.part_group }}</span>
+                        </span>
+                        <span v-if="request.type_of_part">
+                          Type of part:
+                          <span class="bold">{{ request.type_of_part }}</span>
+                        </span>
+                        <span v-if="request.part_condition">
+                          Condition:
+                          <span class="bold">{{ request.part_condition }}</span>
+                        </span>
+                        <span v-if="request.part_code">
+                          Part code:
+                          <span class="bold">{{ request.part_code }}</span>
+                        </span>
+                        <span v-if="request.car_type">
+                          Car type:
+                          <span class="bold">{{ request.car_type }}</span>
+                        </span>
+                        <span v-if="request.car_make">
+                          Car make:
+                          <span class="bold">{{ request.car_make }}</span>
+                        </span>
+                        <span v-if="request.car_model">
+                          Car model:
+                          <span class="bold">{{ request.car_model }}</span>
+                        </span>
+                        <span v-if="request.car_year">
+                          Car year:
+                          <span class="bold">{{ request.car_year }}</span>
+                        </span>
                       </div>
-
-                      <div class="spare-part-requests-btn-wrapper">
-                        <NuxtLink v-if="loggedIn" :to="'/offer-page/' + request._id" class="suggest-button xl-green-btn"
-                          >Suggest your variant</NuxtLink
-                        >
-                        <NuxtLink v-else :to="'/sign-in'" class="suggest-button xl-green-btn"
-                          >Sign in and suggest your variant</NuxtLink
-                        >
-                        <span class="close-content-btn blue-btn" @click="hideAllContentOfSingleRequest"
-                          >Hide content</span
-                        >
+                      <div class="single-request-details-right">
+                        <span v-if="request.fuel_type">
+                          Fuel type:
+                          <span class="bold">{{ request.fuel_type }}</span>
+                        </span>
+                        <span v-if="request.engine_volume">
+                          Engine volume:
+                          <span class="bold">{{ request.engine_volume }}</span>
+                        </span>
+                        <span v-if="request.car_body">
+                          Car body:
+                          <span class="bold">{{ request.car_body }}</span>
+                        </span>
+                        <span v-if="request.vin_code">
+                          VIN code:
+                          <span class="bold">{{ request.vin_code }}</span>
+                        </span>
+                        <span v-if="request.comment">
+                          User comment:
+                          <span class="bold">{{ request.comment }}</span>
+                        </span>
+                        <span v-if="request.city">
+                          State/City:
+                          <span class="bold">{{ request.city }}</span>
+                        </span>
+                        <span v-if="request.name">
+                          Name: <span class="bold">{{ request.name }}</span>
+                        </span>
                       </div>
                     </div>
+
+                    <div class="spare-part-requests-btn-wrapper">
+                      <NuxtLink v-if="loggedIn" :to="'/offer-page/' + request._id" class="suggest-button xl-green-btn"
+                        >Suggest your variant</NuxtLink
+                      >
+                      <NuxtLink v-else :to="'/sign-in'" class="suggest-button xl-green-btn"
+                        >Sign in and suggest your variant</NuxtLink
+                      >
+                      <span class="close-content-btn blue-btn" @click="hideAllContentOfSingleRequest"
+                        >Hide content</span
+                      >
+                    </div>
                   </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <br />
-      <paginate
-        v-model="currentPage"
-        :page-count="numPages"
-        :page-range="5"
-        :click-handler="rewriteChunkOfRequests"
-        :container-class="'pagination'"
-        prev-text="<"
-        next-text=">"
-      />
     </div>
+    <br />
+    <paginate
+      v-model="currentPage"
+      :page-count="numPages"
+      :page-range="5"
+      :click-handler="rewriteChunkOfRequests"
+      :container-class="'pagination'"
+      prev-text="<"
+      next-text=">"
+    />
   </div>
 </template>
 
