18e452d2f9b14651ebc2bcb6a8ec982ed1ff8acf
Oleg Zhuravko <o.zhuravko@gmail.com>
2024-02-22 18:25:04 +0200

    change view of successfull account activated page

--- client/pages/activation-success.vue
+++ client/pages/activation-success.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="content-wrapper activating-screen">
-    <h1>Account Activated</h1>
-    <div>
+  <div class="content-wrapper">
+    <div class="error-page-content">
+      <h1>Account Activated</h1>
       <p>
         Congratulation! Your Account activated succesful. Now you can use your account and use the service to the
         fullest.
       </p>
+      <div class="links-nav">
+        <NuxtLink to="/part-request">Find Parts</NuxtLink>
+        <NuxtLink to="/all-spare-part-requests">Suggest Your Parts</NuxtLink>
+        <NuxtLink to="/how-it-work">How It Work</NuxtLink>
+        <NuxtLink to="/">Home</NuxtLink>
+      </div>
+    </div>
+    <div class="error-img-wrapper">
+      <NuxtImg src="/images/shaking-hands.jpg" title="shaking-hands" />
     </div>
   </div>
+  <TheFooter />
 </template>
 
 <script setup>
@@ -26,42 +36,75 @@ useHead({
 <style lang="scss" scoped>
 @import "@/assets/styles/_variables.scss";
 
-.activating-screen {
-  padding: 0 0 20vh 0;
-  h1 {
-    font-size: 40px;
-    margin-bottom: 40px;
-    font-weight: 300;
-    text-align: center;
+.content-wrapper {
+  min-height: 60vh;
+  font-size: 16px;
+  display: flex;
+  justify-content: space-between;
 
-    @media (max-width: 834px) {
-      font-size: 28px;
+  @media (max-width: 1070px) {
+    flex-direction: column;
+    align-items: center;
+  }
+  .error-page-content {
+    width: 45%;
+
+    @media (max-width: 1070px) {
+      width: 70%;
+      text-align: center;
     }
 
     @media (max-width: 768px) {
-      margin: 20px auto;
+      width: 100%;
+      text-align: start;
     }
 
-    @media (max-width: 382px) {
-      font-size: 24px;
+    h1 {
+      font-size: 50px;
+      font-weight: 300;
+      margin-bottom: 10px;
+    }
+
+    p {
+      font-size: 20px;
+      font-weight: 300;
+      color: $gray-800;
+    }
+
+    .links-nav {
+      display: flex;
+      justify-content: space-between;
+
+      @media (max-width: 768px) {
+        flex-direction: column;
+      }
+
+      a {
+        text-decoration: none;
+        color: $blue;
+        font-weight: 500;
+
+        @media (max-width: 768px) {
+          font-weight: 400;
+          margin-bottom: 10px;
+        }
+      }
     }
   }
 
-  p {
+  .error-img-wrapper {
+    width: 45%;
+    display: flex;
+    justify-content: flex-end;
     margin-top: 30px;
-    font-size: 20px;
 
-    @media (max-width: 768px) {
-      margin-top: 20px;
+    @media (max-width: 1070px) {
+      width: 100%;
+      justify-content: center;
     }
 
-    @media (max-width: 382px) {
-      font-size: 18px;
-    }
-    a {
-      text-decoration: none;
-      color: $blue;
-      font-weight: 600;
+    img {
+      width: auto;
     }
   }
 }
