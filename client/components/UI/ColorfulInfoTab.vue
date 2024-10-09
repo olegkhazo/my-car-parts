40ad6ac15235f8cb85913d2aa04c0885533e48af
Oleg Zhuravko <o.zhuravko@gmail.com>
2024-10-09 19:08:53 +0200

    [WIP] add infoTab ui component

---
+++ client/components/UI/ColorfulInfoTab.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  text: String,
+  bgColor: String,
+  textColor: String,
+});
+</script>
+
+<template>
+  <span :style="{ backgroundColor: props.bgColor, color: props.textColor }">
+    {{ props.text }}
+  </span>
+</template>
+
+<style lang="scss" scoped>
+span {
+  padding: 3px 12px;
+  border-radius: 5px;
+  background: opacity(0.9);
+}
+</style>
