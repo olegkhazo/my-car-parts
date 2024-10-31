7eec428212291822a9b4b4bddd8e334807ba9fbe
Oleg Zhuravko <o.zhuravko@gmail.com>
2024-10-31 13:00:23 +0100

    [WIP] market index page

---
+++ client/market/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div><h1>Shop is there</h1></div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ client/pages/market/index.vue
@@ -0,0 +1,30 @@
+<script setup>
+definePageMeta({
+  layout: "default",
+});
+
+useHead({
+  title: "MyNextParts - the biggest used car parts market",
+  meta: [
+    {
+      name: "Market - home page",
+      content: "Home page of a used carparts market",
+    },
+  ],
+});
+
+//fetching all requests
+const { data: vmAutoCollection, error } = await useFetch(`${API_URL}vm-auto-list`);
+
+if (error) {
+  console.log("Something wrong:" + error.value);
+}
+</script>
+
+<template>
+  <div>
+    <h1>Shop is there</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
